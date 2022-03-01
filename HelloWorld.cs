08139ed9ac812b725b1a8e8fa40099adbe56d7f0
yonatanZoarets <73195675+yonatanZoarets@users.noreply.github.com>
2022-03-01 14:13:47 +0200

    Add project files.

---
+++ HelloWorld.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using System.ServiceModel;
+
+namespace ClassLibrary3
+{
+    public class HelloWorld: IPlugin
+    {
+            public void Execute(IServiceProvider serviceProvider)
+            {
+                // Obtain the tracing service
+                ITracingService tracingService =
+                (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+                // Obtain the execution context from the service provider.  
+                IPluginExecutionContext context = (IPluginExecutionContext)
+                    serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+                // The InputParameters collection contains all the data passed in the message request.  
+                if (context.InputParameters.Contains("Target") &&
+                    context.InputParameters["Target"] is Entity)
+                {
+                    // Obtain the target entity from the input parameters.  
+                    Entity entity = (Entity)context.InputParameters["Target"];
+
+                    // Obtain the organization service reference which you will need for  
+                    // web service calls.  
+                    IOrganizationServiceFactory serviceFactory =
+                        (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                    IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                    try
+                    {
+                    string firstName = "";
+                    if (entity.Attributes.Contains("firstname"))
+                    {
+                        firstName = entity.Attributes["firstname"].ToString();
+                    }
+                        string lastNmae = entity.Attributes["lastname"].ToString();
+
+                        entity.Attributes.Add("description", "Hello World" + firstName + " " + lastNmae);
+                    }
+
+                    catch (FaultException<OrganizationServiceFault> ex)
+                    {
+                        throw new InvalidPluginExecutionException("An error occurred in FollowUpPlugin.", ex);
+                    }
+
+                    catch (Exception ex)
+                    {
+                        tracingService.Trace("FollowUpPlugin: {0}", ex.ToString());
+                        throw;
+                    }
+                }
+            }
+    }
+}
---
+++ TaskCreate.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using System.ServiceModel;
+
+namespace ClassLibrary3
+{
+    class TaskCreate : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Obtain the tracing service
+            ITracingService tracingService =
+            (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Obtain the execution context from the service provider.  
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // The InputParameters collection contains all the data passed in the message request.  
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+                // Obtain the target entity from the input parameters.  
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                // Obtain the organization service reference which you will need for  
+                // web service calls.  
+                IOrganizationServiceFactory serviceFactory =
+                    (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                try
+                {
+                    Entity record = new Entity("task");
+
+                    record.Attributes.Add("subject","follow up");
+                    record.Attributes.Add("statecode", "in process");
+                    record.Attributes.Add("description", "follow up with my contact");
+                    record.Attributes.Add("prioritycode", new OptionSetValue(2));
+                    //
+                    record.Attributes.Add("regardingobjectid", entity.ToEntityReference());
+                    //record.Attributes.Add("regardingobjectid", new EntityReference("contact",entity.Id));
+                    Guid guid = service.Create(record);
+                }
+
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("An error occurred in FollowUpPlugin.", ex);
+                }
+
+                catch (Exception ex)
+                {
+                    tracingService.Trace("FollowUpPlugin: {0}", ex.ToString());
+                    throw;
+                }
+            }
+        }
+    }
+}
